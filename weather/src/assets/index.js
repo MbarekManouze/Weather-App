5be3c1cadea377ae6b4366166d8f44520a9357e6
MbarekManouze <ecloesbac2020@gmail.com>
2024-01-02 16:14:05 +0100

    Routing Next

---
+++ weather/src/assets/index.js
@@ -0,0 +1,12 @@
+import React from './react.svg';
+import Weather from './weather.png';
+import Wind from './wind.png'
+import List from './list.png'
+
+export {
+    React,
+    Weather,
+    Wind,
+    List,
+    
+};
